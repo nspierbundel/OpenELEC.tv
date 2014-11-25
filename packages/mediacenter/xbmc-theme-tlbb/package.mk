7cece8322f3876e3753ea083c22348230c1495b2
Alex Deryskyba <alex@codesnake.com>
2014-11-25 23:37:01 +0200

    [TLBB] Add the TLBB skin and set it as default

---
+++ packages/mediacenter/xbmc-theme-tlbb/package.mk
@@ -0,0 +1,33 @@
+################################################################################
+#      This file is part of OE-TLBB - http://thelittleblackbox.com
+#      Copyright (C) 2014 Alex Deryskyba (alex@codesnake.com)
+#
+#  OpenELEC is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  OpenELEC is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with OpenELEC.  If not, see <http://www.gnu.org/licenses/>.
+################################################################################
+
+PKG_NAME="xbmc-theme-tlbb"
+PKG_VERSION=""
+PKG_REV="1"
+PKG_ARCH="any"
+PKG_LICENSE="various"
+PKG_SITE="http://thelittleblackbox.com"
+PKG_URL=""
+PKG_DEPENDS_TARGET="toolchain"
+PKG_PRIORITY="optional"
+PKG_SECTION="virtual"
+PKG_SHORTDESC="xbmc-theme-tlbb: Metapackage for various The Little Black Box skin"
+PKG_LONGDESC="xbmc-theme-tlbb: Metapackage for various The Little Black Box skin"
+
+PKG_IS_ADDON="no"
+PKG_AUTORECONF="no"
